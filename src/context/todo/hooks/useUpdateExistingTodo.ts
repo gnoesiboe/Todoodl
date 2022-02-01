27dae280685bcfe3ac3fa161f029ee74317e82e7
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2022-02-01 06:58:31 +0100

    Persist updates to todo

--- src/context/todo/hooks/useUpdateExistingTodo.ts
+++ src/context/todo/hooks/useUpdateExistingTodo.ts
@@ -2,18 +2,23 @@ import { useCallback } from 'react';
 import produce from 'immer';
 import { TodoCollection } from '../../../model/todo';
 import { UpdateTodoHandler } from './useManageTodosState';
+import { persistTodo } from '../../../firebase/repository/todoRepository';
+import { useLoggedInUser } from '../../../auth/AuthContext';
 
 export default function useUpdateExistingTodo(
     todos: TodoCollection | null,
     setTodos: (todos: TodoCollection | null) => void,
 ): UpdateTodoHandler {
+    const user = useLoggedInUser();
+
     return useCallback<UpdateTodoHandler>(
         async (uuid, changes) => {
-            const newTodos = produce<TodoCollection | null>(todos, (newTodos) => {
-                if (newTodos === null) {
-                    throw new Error('Expecting there to be todos at this point');
-                }
+            if (!todos) {
+                throw new Error('Expecting there to be todos at this point');
+            }
 
+            // update in-memory state
+            const newTodos = produce<TodoCollection>(todos, (newTodos) => {
                 const existingTodoIndex = newTodos.findIndex((cursorTodo) => cursorTodo.uuid === uuid);
 
                 if (existingTodoIndex === -1) {
@@ -25,11 +30,23 @@ export default function useUpdateExistingTodo(
                     ...changes,
                 };
             });
-
             setTodos(newTodos);
 
-            // @todo push changes to backend
+            // update server state
+            const existingTodoIndex = newTodos.findIndex((cursorTodo) => cursorTodo.uuid === uuid);
+            const updatedTodo =
+                existingTodoIndex !== -1
+                    ? {
+                          ...todos[existingTodoIndex],
+                          ...changes,
+                      }
+                    : null;
+
+            if (updatedTodo) {
+                // @todo instead of overriding the entire todo, only push updates
+                await persistTodo(updatedTodo, user);
+            }
         },
-        [setTodos, todos],
+        [setTodos, todos, user],
     );
 }
