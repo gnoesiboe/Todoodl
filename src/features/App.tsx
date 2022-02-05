f3dba97b0a91f1201a302282ecd2ef39501085c4
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2022-02-05 14:49:53 +0100

    Show keyboard shortcut after timeout so that it doesn't flash by while loading the todos

--- src/features/App.tsx
+++ src/features/App.tsx
@@ -3,15 +3,18 @@ import TodoOverview from './components/todoOverview/TodoOverview';
 import KeyboardShortcutOverview from './components/keyboardShortcutOverview/KeyboardShortcutOverview';
 import { TodoContextProvider } from '../context/todo/TodoContext';
 import { AuthContextProvider } from '../auth/AuthContext';
+import useSwitchAfterTimeout from '../hooks/useSwitchAfterTimeout';
 
 function App() {
+    const keyboardShortcutOverviewVisible = useSwitchAfterTimeout(false, 2000);
+
     return (
         <AuthContextProvider>
             <main className="max-w-4xl mx-auto md:mt-4 space-y-10">
                 <TodoContextProvider>
                     <TodoOverview />
                 </TodoContextProvider>
-                <KeyboardShortcutOverview />
+                {keyboardShortcutOverviewVisible && <KeyboardShortcutOverview />}
             </main>
         </AuthContextProvider>
     );
