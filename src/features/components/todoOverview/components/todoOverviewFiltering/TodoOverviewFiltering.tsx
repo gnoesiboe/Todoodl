676cba9b7ebb686a7ef2084080f1ad5015d29530
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2022-02-06 09:45:26 +0100

    Fixed styling issue with filters on mobile

--- src/features/components/todoOverview/components/todoOverviewFiltering/TodoOverviewFiltering.tsx
+++ src/features/components/todoOverview/components/todoOverviewFiltering/TodoOverviewFiltering.tsx
@@ -33,19 +33,26 @@ const TodoOverviewFiltering: VFC<Props> = ({
     toggleTag,
     className: additionalClassName,
 }) => {
-    const { visible, show, hide } = useToggleVisibility(false);
+    const { visible, show, hide } = useToggleVisibility(true); // @todo revert
 
     const options = useHandleFilterOptions(todos);
 
     const appliedFilterCount = determineAmountOfAppliedFilters(appliedFilters);
 
-    const containerClassName = createClassName('border-b-2', additionalClassName);
+    const containerClassName = createClassName('border-b-2', additionalClassName, {
+        'pb-5': visible,
+    });
 
     return (
         <div className={containerClassName}>
             {visible ? (
-                <div className="flex items-start justify-between pb-10">
-                    <div className="flex gap-10">
+                <>
+                    <div className="text-right">
+                        <Button variant="unstyled" onClick={hide}>
+                            <X />
+                        </Button>
+                    </div>
+                    <div className="flex gap-x-10 gap-y-5 flex-wrap">
                         <PrioritiesChoiceList
                             appliedFilters={appliedFilters.priorities}
                             filterMap={options.priorities}
@@ -62,10 +69,7 @@ const TodoOverviewFiltering: VFC<Props> = ({
                             filterMap={options.tags}
                         />
                     </div>
-                    <Button variant="unstyled" onClick={hide}>
-                        <X />
-                    </Button>
-                </div>
+                </>
             ) : (
                 <Button variant="link" className="flex gap-2 items-center" deflated="x" onClick={show}>
                     <Filter size={14} /> Filter {appliedFilterCount > 0 && `(${appliedFilterCount})`}
--- src/features/components/todoOverview/components/todoOverviewFiltering/components/SectionContainer.tsx
+++ src/features/components/todoOverview/components/todoOverviewFiltering/components/SectionContainer.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const SectionContainer: FC = ({ children }) => {
-    return <div className="space-y-2">{children}</div>;
+    return <div className="space-y-2 min-w-fit">{children}</div>;
 };
 
 export default SectionContainer;
